e6b12007423e17067da48d40bb74c697046d98aa
Mubzz09 <143597850+DevMubzly@users.noreply.github.com>
2025-09-03 13:32:19 +0300

    hehehehh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      // Ensure old link path also works
+      { source: "/CV.pdf", destination: "/myCV.pdf", permanent: false },
+    ];
+  },
 };
 
 export default nextConfig;
